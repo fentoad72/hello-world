c8056f46abdd3845d89bf3fe36fec50deed50a9c
Todd Arbetter <arbetter@ucar.edu>
2020-03-03 14:18:29 -0700

    added Mr Dobalino

--- hello_world.py
+++ hello_world.py
@@ -2,8 +2,10 @@
 
 print("The quick brown fox jumped over the lazy dog.")
 
-print()
-print()
+print
+print
 print("It isn't our intention that the people are intending.")
-
+print
+print
+print("Mister Dobolino, Mister Bob Dobalino")
 
