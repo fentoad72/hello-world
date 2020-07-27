a13a32e5efb14a1e05b22c2281a40e075ceb40b9
Todd Arbetter <arbetter@ucar.edu>
2020-07-27 15:41:02 -0600

    commented out 'import numpy'

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
-import numpy
+#import numpy
 
 print("The quick brown fox jumped over the lazy dog.")
 print("It isn't our intention that the people are intending.")
 print("Mister Dobolino, Mister Bob Dobalino")
-
-end
