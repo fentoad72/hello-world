3fbf0aac14e27b6d92dff9974ff8da2254423c41
Todd Arbetter <arbetter@ucar.edu>
2020-07-27 15:25:24 -0600

    removed blank print statements from hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -2,10 +2,6 @@
 
 print("The quick brown fox jumped over the lazy dog.")
 
-print
-print
 print("It isn't our intention that the people are intending.")
-print
-print
 print("Mister Dobolino, Mister Bob Dobalino")
 
