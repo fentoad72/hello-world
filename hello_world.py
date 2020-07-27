f182191f92758d55656d43bd885673fb4f7906a6
Todd Arbetter <arbetter@ucar.edu>
2020-07-27 15:38:17 -0600

    changed docstring to numpy

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
-import docstring
+import numpy
 
 print("The quick brown fox jumped over the lazy dog.")
 print("It isn't our intention that the people are intending.")
 print("Mister Dobolino, Mister Bob Dobalino")
+
+end
